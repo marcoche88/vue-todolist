2b973a9eeecf2fb375f9df97a13ace9a09ad1dba
marcoche88 <marco.che@hotmail.it>
2021-07-29 17:01:25 +0200

    add current date

--- js/script.js
+++ js/script.js
@@ -40,4 +40,11 @@ const app = new Vue({
             this.newDate = "";
         }
     },
+    created: function currentDate() {
+        const d = new Date();
+        const day = d.getDate();
+        const year = d.getFullYear();
+        const month = ((d.getMonth() + 1).toString().length === 1) ? `0${d.getMonth() + 1}` : d.getMonth() + 1;
+        this.newDate = `${year}-${month}-${day}`
+    },
 });
