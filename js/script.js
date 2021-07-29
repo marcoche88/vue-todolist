0bb169dfe95ec4ad4ad94a4d9708ab3e5c685604
marcoche88 <marco.che@hotmail.it>
2021-07-29 15:13:53 +0200

    boilerplate + css + js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+Descrizione:
+Rifare l'esercizio della to do list come fatto assieme in classe:
+- stampare in pagina un item per ogni elemento contenuto in un array
+- ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+- predisporre un input per aggiungere un nuovo item alla lista: ciccando su un pulsante,
+il testo digitato viene aggiunto alla lista.
+BONUS 1: Fare un controllo di validazione per non aggiungere degli item vuoti.
+BONUS 2: Dedicarsi alla parte grafica aggiungendo un tocco personale e gestendo anche
+il mobile.
+*/
